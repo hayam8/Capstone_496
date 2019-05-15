542c54469e4b42ec237fc01b2fc713a13b36ce12
Jesus Govela <kechu.govela@gmail.com>
2019-05-14 21:40:24 -0700

    Added more levels

--- leveldatabase.py
+++ leveldatabase.py
@@ -21,8 +21,6 @@ level4Shortcuts = [shortcutdatabase.openSearch, shortcutdatabase.openNewWindow, 
 level4 = Level("Level 4", level4Description, level4Shortcuts, 5)
 """
 
-level1Description = 
-
 level1Description = "Open the Notepad application"
 level1Actions = [shortcutdatabase.openSearch, 'n', 'o', 't', 'e','p', 'a', 'd', 'Enter']
 level1 = Level("Level 1", level1Description, level1Actions, 5)
@@ -31,19 +29,22 @@ level2Description = "Pin this application to the left side of the screen"
 level2Shortcuts = [shortcutdatabase.pinLeft]
 level2 = Level("Level 2", level2Description, level2Shortcuts, 5)
 
-level3Description = "Fullscreen this application"
-level3Shortcuts = [shortcutdatabase.?]
-level3 = Level("Level 3", level3Description, level2Shortcuts, 5)
+level3Description = "Maximize this window"
+level3Shortcuts = [shortcutdatabase.maximize]
+level3 = Level("Level 3", level3Description, level3Shortcuts, 5)
 
-level4Description = "Tab through all your open windows using Alt-Tab"
-level4Shortcuts = [shortcutdatabase.switchTabs]
-level4  = Level("Level 4", level4Description, level4Shortcuts, 5)
+level4Description = "Shrink this window"
+level4Shortcuts = [shortcutdatabase.minimize]
+level4 = Level("Level 4", level4Description, level4Shortcuts, 5)
 
-level5Description = "Tab through all your open windows using CRTL-Alt-Tab"
-level5Shortcuts = [shortcutdatabase.?]
+level5Description = "Tab through all your open windows using Alt-Tab"
+level5Shortcuts = [shortcutdatabase.switchTabs]
 level5  = Level("Level 5", level5Description, level5Shortcuts, 5)
 
-level6Description = ""
+level6Description = "Tab through all your open windows using CRTL-Alt-Tab"
+level6Shortcuts = [shortcutdatabase.switchBetweenAllTabs]
+level6  = Level("Level 6", level6Description, level6Shortcuts, 5)
+
 
 """
 level database tests
